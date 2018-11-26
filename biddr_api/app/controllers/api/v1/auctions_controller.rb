5801dd889cde5056882d6f83a7d86fdfeb75b257
Orsolya Torokne Bus <orsolyabus@gmail.com>
2018-11-26 09:56:36 -0800

    generate api controller for auctions

---
+++ biddr_api/app/controllers/api/v1/auctions_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::AuctionsController < ApplicationController
+
+  def create
+
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+end
--- biddr_api/config/routes.rb
+++ biddr_api/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'auctions/show'
+      get 'auctions/index'
+      get 'auctions/destroy'
+      post 'auctions/create'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
