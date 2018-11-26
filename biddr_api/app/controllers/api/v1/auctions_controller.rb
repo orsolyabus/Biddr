44c497de48d9bd0bbb95956b8ee860fe78d53c4a
Orsolya Torokne Bus <orsolyabus@gmail.com>
2018-11-26 10:19:44 -0800

    auctions CRUD

--- biddr_api/app/controllers/api/v1/auctions_controller.rb
+++ biddr_api/app/controllers/api/v1/auctions_controller.rb
@@ -1,15 +1,37 @@
 class Api::V1::AuctionsController < ApplicationController
+  # skip_before_action :verify_authenticity_token
 
   def create
-
+    auction = Auction.new auction_params
+    if auction.save
+      render json: auction
+    else
+      render json: {errors: auction.errors.full_messages}
+    end
   end
 
   def show
+    render json: auction
   end
 
   def index
+    auctions = Auction.order created_at: :desc
+    render json: auctions
   end
 
   def destroy
+    auction.destroy
+    render json: {status: :success}
   end
+
+  private
+
+  def auction
+    Auction.find params[:id]
+  end
+
+  def auction_params
+    params.require(:auction).permit(:title, :details, :ends_on, :reserved_price)
+  end
+
 end
