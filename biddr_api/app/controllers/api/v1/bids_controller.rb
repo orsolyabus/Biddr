3dc4c7833f1d4a1aceb12bed1e4fd39213168a46
Orsolya Torokne Bus <orsolyabus@gmail.com>
2018-11-26 11:51:09 -0800

    implement session create/destroy

--- biddr_api/app/controllers/api/v1/bids_controller.rb
+++ biddr_api/app/controllers/api/v1/bids_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::BidsController < ApplicationController
+class Api::V1::BidsController < Api::ApplicationController
   def create
     bid = Bid.new bid_params
     bid.auction = Auction.find params[:auction_id]
--- biddr_api/config/routes.rb
+++ biddr_api/config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
+      resource :sessions, only: [:create, :destroy]
       resources :auctions, only:[:create, :show, :index, :destroy] do
         resources :bids, only: [:create]
       end
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
