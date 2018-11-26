bd99b8ea0ca70eca33f815bd0e52f3243c10404a
Orsolya Torokne Bus <orsolyabus@gmail.com>
2018-11-26 12:55:57 -0800

    create Welcome Page

---
+++ biddr/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import WelcomePage from "./WelcomePage";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <WelcomePage/>
+      </div>
+    );
+  }
+}
+
+export default App;
