bceff569ce7865baf7c7938b45dbf8c8c489a960
Orsolya Torokne Bus <orsolyabus@gmail.com>
2018-11-26 19:19:40 -0800

    reverse bids order on auction show page

--- biddr_api/app/serializers/auction_serializer.rb
+++ biddr_api/app/serializers/auction_serializer.rb
@@ -2,4 +2,8 @@ class AuctionSerializer < ActiveModel::Serializer
   attributes :title, :details, :reserved_price, :ends_on, :id
   belongs_to :user
   has_many :bids
+
+  def bids
+    object.bids.order(created_at: :desc)
+  end
 end
